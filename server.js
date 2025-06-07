1f84b90c501a9b456f3e8ed8f4281c2ca4840375
Rohitshukla1997 <rohitsanjayshukla@gmail.com>
2025-06-07 14:12:51 +0530

    Refactor database connection to use mongoose directly in server startup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require("cors");
 const connectDB = require('./Database/dbConnect');
 const route = require("./Routes/route");
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 const port = process.env.PORT;
@@ -35,6 +36,6 @@ app.post('/data', (req, res) => {
 app.listen(port, async () => {
     // const key = Buffer.from(process.env.ENCRYPTION_KEY, "utf8");
     // console.log("Key Length (bytes):", key.length); // for see keyword bit size
-    await connectDB();
+    await mongoose.connect(process.env.DB_URI)
     console.log(`Server is running on http://localhost:${port}`);
 });
